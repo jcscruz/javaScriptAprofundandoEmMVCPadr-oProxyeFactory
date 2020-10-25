1ef3bf81a7521dea601eb90e990918c74dcba942
Julio <julio100@gmail.com>
2020-10-25 07:59:32 -0300

    Correçao http service

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,26 +1,23 @@
-class HttpService{
+class HttpService {
 
-    get(url){
+    get(url) {
 
         return new Promise((resolve, reject) => {
+
             let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'negociacoes/retrasada')
-            
-            xhr.onreadystatechange = () =>{
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-
-                        resolve(JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))                    
-                            
-                    }else{
-                        console.log("Erro ao obter negociações do servidor.")
-                        reject('Erro na importação das negociações da retrasada')
+            xhr.open('GET', url);
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4) {
+                    if(xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        console.log(xhr.responseText);
+                        reject(xhr.responseText);
                     }
-                }        
+                }
             }
-            xhr.send()
-        })
 
+            xhr.send();
+        });
     }
 }
